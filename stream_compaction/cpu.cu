de7ece0131c1f153dcf5d8dbf89f44fd83b32b81
Akula <aniakula92@gmail.com>
2026-09-13 07:18:50 -0400

    finished part 1 CPU impl

--- stream_compaction/cpu.cu
+++ stream_compaction/cpu.cu
@@ -19,8 +19,19 @@ namespace StreamCompaction {
          */
         void scan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+			odata[0] = 0;
+            for (int i = 0; i < n-1; i++) {
+				odata[i + 1] = odata[i] + idata[i];
+            }
             timer().endCpuTimer();
+        }
+
+		//remove timer so compactWithScan can use it without double timing
+        void scan_no_timer(int n, int* odata, const int* idata) {
+            odata[0] = 0;
+            for (int i = 0; i < n - 1; i++) {
+                odata[i + 1] = odata[i] + idata[i];
+            }
         }
 
         /**
@@ -30,10 +41,24 @@ namespace StreamCompaction {
          */
         int compactWithoutScan(int n, int *odata, const int *idata) {
             timer().startCpuTimer();
-            // TODO
+			int outputIndex = 0;
+            for (int i = 0; i < n; i++) {
+                if (idata[i] != 0) {
+                    odata[outputIndex] = idata[i];
+					outputIndex++;
+                }
+            }
             timer().endCpuTimer();
-            return -1;
+            return outputIndex;
         }
+
+        void scatter(int n, int *odata, const int *idata, const int *mask, const int *indices) {
+            for (int i = 0; i < n; i++) {
+                if (mask[i] == 1) {
+                    odata[indices[i]] = idata[i];
+                }
+            }
+		}
 
         /**
          * CPU stream compaction using scan and scatter, like the parallel version.
@@ -41,10 +66,26 @@ namespace StreamCompaction {
          * @returns the number of elements remaining after compaction.
          */
         int compactWithScan(int n, int *odata, const int *idata) {
+            int* mask = new int[n];
+            int* outputIndices = new int[n];
+
             timer().startCpuTimer();
-            // TODO
+			//map to boolean array
+            for (int i = 0; i < n; i++) {
+                mask[i] = (idata[i] != 0) ? 1 : 0;
+			}
+			
+			scan_no_timer(n, outputIndices, mask);
+            scatter(n, odata, idata, mask, outputIndices);
+
+            //if last elem is 0, last ind == num valid elems (index of the next valid elem if it existed)
+            //if last elem is i, last ind == the index of the last valid elem so we add 1
+			int count = outputIndices[n - 1] + mask[n - 1];
             timer().endCpuTimer();
-            return -1;
+
+            delete[] mask;
+            delete[] outputIndices;
+            return count;
         }
     }
 }
